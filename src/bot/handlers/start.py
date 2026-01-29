f855a5b1bc944de8ee090fec204f44832948fd30
Qeenky <kiritorf79@gmail.com>
2026-01-29 07:47:10 +0500

    Added help command

--- src/bot/handlers/start.py
+++ src/bot/handlers/start.py
@@ -8,3 +8,8 @@ user_router = Router()
 @user_router.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("Привет!")
+
+# TODO: Обновлять содержание по мере создания функций
+@user_router.message(Command("help"))
+async def cmd_help(message: types.Message):
+    await message.answer("Помощь\n"+"="*30+"\n\n"+"Команды:")
